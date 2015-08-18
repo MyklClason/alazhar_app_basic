6ffacfd4573c8bc4ca3df2e7753884f6734bc1b4
shabbirun <shabbirun@gmail.com>
2015-08-18 22:55:46 +0530

    three admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if@user.save
-			session[:user_id] = @user.id
-			redirect_to root_url
+			redirect_to users_path
 			flash[:info] = "User successfully created!"
 		else
 			render "new"
